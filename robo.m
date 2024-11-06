81c529172a2f6a971bd22db68fd712cc3105fae5
David Fales <davidfalesct@gmail.com>
2024-11-06 09:31:41 -0700

    add

--- robo.m
+++ robo.m
@@ -1,94 +1,113 @@
-%brick = ConnectBrick('ANT');
+% Connect and configure brick
+brick = ConnectBrick('ANT');
 done = 0;
-% brick.setColorMode(2,1); % port 2, ambient light mode
-% Color sensor must be connected t port 2, ultrasonic to D, 
-% touch sensor to A, 
-% Motors are B and C.
+
+% Function to move motors forward
+function moveForward(brick, speed, duration)
+    brick.MoveMotor('B', speed);
+    brick.MoveMotor('C', speed);
+    pause(duration);
+end
+
+% Function to stop all motors
+function stopMotors(brick, mode)
+    brick.StopAllMotors(mode);
+end
+
+% Function to turn right
+function turnRight(brick, speed, duration)
+    brick.MoveMotor('B', -speed);
+    brick.MoveMotor('C', speed);
+    pause(duration);
+end
+
+% Function to turn left
+function turnLeft(brick, speed, duration)
+    brick.MoveMotor('B', speed);
+    brick.MoveMotor('C', -speed);
+    pause(duration);
+end
+
+% Function to beep a specified number of times
+function beepMultiple(brick, times)
+    for i = 1:times
+        brick.beep();
+        pause(0.5);
+    end
+end
+
+% Function to adjust movement based on distance
+function fixMovement(brick, oldDist, newDist)
+    if newDist < oldDist
+        % Moving closer to the wall, so turn slightly right
+        disp("Adjusting: Getting closer to wall, turning right");
+        turnRight(brick, 20, 0.5);
+    elseif newDist > oldDist
+        % Moving away from the wall, so turn slightly left
+        disp("Adjusting: Moving away from wall, turning left");
+        turnLeft(brick, 20, 0.5);
+    elseif abs(newDist - oldDist) > 50
+        % Significant distance increase, indicating an opening
+        disp("Opening detected, adjusting to move forward");
+        moveForward(brick, 40, 1);
+    end
+end
+
+% Main loop
 while done == 0
-    newDist = 0;
-    oldDist = 0;
+    oldDist = brick.UltrasonicDist(4); % Initial distance check
     
     while brick.TouchPressed(3) == 0
         color = brick.ColorCode(1);
-        distance = brick.UltrasonicDist(4);
-        disp("distance: " + distance);
-        disp("color: " + color)
-        if distance > 102                                              
-            brick.MoveMotor('B', 30);
-            brick.MoveMotor('C', -30);
-            
-            pause(1);
-            brick.MoveMotor('B', 40);
-            brick.MoveMotor('C', 40);
-            %could need if
-            
-            pause(3);
-        end
-        brick.MoveMotor('B', 40);
-        brick.MoveMotor('C', 40);
-        if color == 5 % red
-            brick.StopAllMotors('Brake');
-            pause(1);
-            brick.MoveMotor('B', 40);
-            brick.MoveMotor('C', 40);
-            pause(2);
-        end 
-        if color == 3 % green
-            brick.beep();
-            pause(0.5);
-            brick.beep();
-            pause(0.5);
-            brick.beep();
-            pause(0.5);
-            brick.StopAllMotors('Brake');
-            pause(1);
-            brick.MoveMotor('B',-50);
-            brick.MoveMotor('C', -50);
-            pause(2); 
-            brick.MoveMotor('B', -25);
-            brick.MoveMotor('C', 25);
-            disp("turning right due to: " + color);
-            pause(1);
-        end
-        if color == 2 % blue
-            brick.beep();
-            pause(0.5);
-            brick.beep();
-            pause(0.5);x
-            brick.StopAllMotors('Brake');
-            pause(1);
-            brick.MoveMotor('B',-50);
-            brick.MoveMotor('C', -50);
-            pause(2); 
-            brick.MoveMotor('B', -25);
-            brick.MoveMotor('C', 25);
-            disp("turning right due to: " + color);
-            pause(1);
-        end
-        if color == 4 % yellow
-            brick.StopAllMotors('Brake');
-            pause(1);
-        end
-    end
-    if brick.TouchPressed(3) == 1
-        brick.StopAllMotors('Brake');
-        pause(2);
-        brick.MoveMotor('B', -30);
-        brick.MoveMotor('C', -30);
-        pause(1);
-        % turn right
-        if distance < 65                                                  
-            brick.MoveMotor('B', -25);
-            brick.MoveMotor('C', 25);
-            disp("turning right due to: " + distance);
-            pause(1);
-        % turn left
-        else 
-            brick.MoveMotor('B', 25);
-            brick.MoveMotor('C', -25);
-            disp("turning left due to: " + +distance);
-            pause(1);
-        end
-    end
+        newDist = brick.UltrasonicDist(4);
+        disp("New distance: " + newDist);
+        disp("Color: " + color);
+
+        % Fix movement based on distance change
+        fixMovement(brick, oldDist, newDist);
         
-end 
+        % Update oldDist for the next loop
+        oldDist = newDist;
+
+        % Check color and respond accordingly
+        switch color
+            case 5 % red
+                stopMotors(brick, 'Brake');
+                pause(1);
+                moveForward(brick, 40, 2);
+                
+            case 3 % green
+                stopMotors(brick, 'Brake');
+                beepMultiple(brick, 3); % Beep three times
+                pause(1);
+                moveForward(brick, -50, 2);
+                turnRight(brick, 25, 1);
+
+            case 2 % blue
+                stopMotors(brick, 'Brake');
+                beepMultiple(brick, 2); % Beep two times
+                pause(1);
+                moveForward(brick, -50, 2);
+                turnRight(brick, 25, 1);
+
+            case 4 % yellow
+                stopMotors(brick, 'Brake');
+                pause(1);
+        end
+    end
+
+    % Action on touch press
+    if brick.TouchPressed(3) == 1
+        stopMotors(brick, 'Brake');
+        pause(2);
+        moveForward(brick, -30, 1);
+        
+        if newDist < 65
+            turnRight(brick, 25, 1);
+            disp("Turning right due to: " + newDist);
+        else
+            turnLeft(brick, 25, 1);
+            disp("Turning left due to: " + newDist);
+        end
+    end
+end
